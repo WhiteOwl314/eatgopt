d9d86091c05714b60d9cc9e18e3de4688c856c78
WhiteOwl314 <parksj914@naver.com>
2020-01-20 11:54:10 +0900

    Create eatgo-api module

---
+++ eatgo-api/src/main/java/api/eatgoapi/EatgoApiApplication.java
@@ -0,0 +1,13 @@
+package api.eatgoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatgoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatgoApiApplication.class, args);
+    }
+
+}
---
+++ eatgo-api/src/test/java/api/eatgoapi/EatgoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.eatgoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatgoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
