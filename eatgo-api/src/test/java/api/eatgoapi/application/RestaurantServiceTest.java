fe8100d8fe4e2cda94ad6059d075ad67f549ef9f
WhiteOwl314 <parksj914@naver.com>
2020-01-20 17:54:59 +0900

    Solve NullPointException

--- eatgo-api/src/test/java/api/eatgoapi/application/RestaurantServiceTest.java
+++ eatgo-api/src/test/java/api/eatgoapi/application/RestaurantServiceTest.java
@@ -4,6 +4,7 @@ import api.eatgoapi.domain.Restaurant;
 import api.eatgoapi.domain.RestaurantRepository;
 import api.eatgoapi.domain.RestaurantRepositoryImpl;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -16,7 +17,7 @@ class RestaurantServiceTest {
     private RestaurantService restaurantService;
     private RestaurantRepository restaurantRepository;
 
-    @Before
+    @BeforeEach
     public void setUp(){
         restaurantRepository = new RestaurantRepositoryImpl();
         restaurantService = new RestaurantService(restaurantRepository);
@@ -27,7 +28,7 @@ class RestaurantServiceTest {
         List<Restaurant> restaurants = restaurantService.getRestaurants();
 
         Restaurant restaurant = restaurants.get(0);
-        assertThat(restaurant.getId()).isEqualTo(1004L);
+        assertThat(restaurant.getId()).isEqualTo(2020L);
     }
 
     @Test
