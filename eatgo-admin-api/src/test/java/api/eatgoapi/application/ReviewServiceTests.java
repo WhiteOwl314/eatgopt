74c3a2483d989fa61b84d126a0600055e4ca9014
WhiteOwl314 <parksj914@naver.com>
2020-02-03 23:48:09 +0900

    Refectoring AdminApi

--- eatgo-admin-api/src/test/java/api/eatgoapi/application/ReviewServiceTests.java
+++ eatgo-admin-api/src/test/java/api/eatgoapi/application/ReviewServiceTests.java
@@ -7,7 +7,12 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 public class ReviewServiceTests {
@@ -24,14 +29,16 @@ public class ReviewServiceTests {
     }
 
     @Test
-    public void addReview(){
-        Review review = Review.builder()
-                .name("JOKER")
-                .score(3)
-                .description("Mat It Da")
-                .build();
-        reviewService.addReview(1004L, review);
+    public void getReviews(){
+        List<Review> mockReviews = new ArrayList<>();
+        mockReviews.add(Review.builder()
+                .description("Cool!").build());
+        given(reviewRepository.findAll()).willReturn(mockReviews);
 
-        verify(reviewRepository).save(any());
+        List<Review> reviews = reviewService.getReviews();
+
+        Review review = reviews.get(0);
+
+        assertThat(review.getDescription()).isEqualTo("Cool!");
     }
 }
